de7aaa739f02a624f5a296c55121fedd02939340
Mateo <96802084+MateoGitIt@users.noreply.github.com>
2022-04-30 21:45:15 -0500

    Add files via upload

---
+++ pagpri.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang=es>
+    
+    <head>
+    
+    <meta charset="UTF-8">
+        <title>Calidad de Aire Colegio San Viator</title>
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    </head>
+    
+    <body>
+    
+        <nav class="navbar navbar-light bg-light shadow rounded">
+            <div class="container-fluid">
+                <a class="navbar-brand fs-2 fw-light">
+                    <img src="Leon.png" alt="" width="60" class="d-inline-block align-text-center">ﾠ
+                    <img src="EFQM.png" alt="" width="80" class="d-inline-block align-text-center">
+                    ㅤㅤㅤㅤㅤㅤCalidad de Aire del Colegio San Viatorㅤㅤㅤㅤㅤﾠㅤ
+                    <img src="Sanvi.png" alt="" width="45" class="d-inline-block align-text-center">ﾠ
+                    <img src="IB.png" alt="" width="60" class="d-inline-block align-text-center">
+                </a>
+            </div>
+        </nav>
+
+        <br>
+    
+        <div class="card text-dark bg-secondary mb-3 shadow" style="width: 950px; margin-left: 175px; --bs-bg-opacity: .2;">
+            <div class="card-body">
+                <p class="card-text">Sea bienvenido a la base de datos de la calidad del aire del Colegio San Viator Bilingüe 
+                Internacional, a continuación encontrará los datos recopilados desde el 1 de mayo de 2022 
+                hasta la actualidad. Tenga en cuenta lo siguiente: la base de datos registra el Índice de 
+                Calidad del Aire (ICA) de 6 polutos presentes en el aire (PM2.5, PM10, O3, NO2, SO2, CO) y 
+                5 variables climáticas (temperatura, presión, humedad, viento, precipitaciones) cada 60 minutos 
+                a lo largo del día en la localidad del colegio. Las clasificaciones de cada valor ICA para los 
+                polutos son delimitadas por los siguientes parámetros: </p>
+
+                ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ<img src="ICAAA.png" width="392"><br><br>
+
+                <p>Los datos presentados son mesurados por el Observatorio Ambiental de Bogotá y distribuidos por 
+                el proyecto World Air Quality Index en <a href="https://aqicn.org/city/colombia/bogota/guaymaral/">AQICN</a>. 
+                Dé uso de esta plataforma para referirse agrandes cantidades de datos con significancia estadística, evaluar la correlación entre 
+                diferentes factores y la calidad del aire en el colegio o localidad en general, evaluar el 
+                impacto de eventos, novedades o políticas en la ciudad de Bogotá sobre el aire, o de cualquier 
+                otra forma que usted vea pertinente.</p>
+
+            </div>
+        </div>
+        
+        <a href="pagtab.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true" style="margin-left:600px">Ingresar</a>
+        
+        <br><br>
+
+    </body>
+    
+    <footer class="bg-light text-center text-lg-start">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            Elaborado por: Mateo Patiño, Gabriel Riaño y Alejadro Apardo
+            <br>Promoción 2022
+        </div>
+    </footer>
+    
+</html>
